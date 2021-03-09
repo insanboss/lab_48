b9a31ab8df7c27d1ab63b6d6d7fff740e6a7b074
Insan <insanboss@gmail.com>
2021-03-09 19:52:28 +0600

    Реализован просмотр товаров на главной странице Реализован детальный просмотр конкретного товара

--- source/product_app/views.py
+++ source/product_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from product_app.models import Product
 
 # Create your views here.
+
+
+def index_view(request):
+    products = Product.objects.exclude(remainder=0).order_by('categories', 'product')
+    context = {
+        'products': products
+    }
+    return render(request, 'index.html', context)
+
+
+def product_view(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', context={'product': product})
